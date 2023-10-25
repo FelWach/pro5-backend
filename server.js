bce202b4f7ac835036fe0631ba4949a61c8ab829
FelWach <s2110238071@fhooe.at>
2023-10-25 18:12:44 +0200

    Updated stuff
    yarn

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const { generateAnswer } = require("./langchain.js");
 const { config } = require("dotenv");
+const bodyParser = require("body-parser");
+
 config();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 let storedData = "";
 let answer = "";
@@ -18,9 +21,11 @@ app.post("/saveData", async (req, res) => {
   res.json({ message: `Data received successfully ${storedData}` });
   answer = await generateAnswer(storedData);
 });
+
 app.get("/", async (req, res) => {
   res.send(`Stored Data: ${answer}`);
 });
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
