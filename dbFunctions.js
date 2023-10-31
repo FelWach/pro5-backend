ffd467c4474728ddf872621b1fe937bb1cd10da5
Oliver <oliverwolfmayr@yahoo.de>
2023-10-31 14:00:03 +0100

    DB_Setup
    DB_Setup

--- dbFunctions.js
+++ dbFunctions.js
@@ -1,42 +1,63 @@
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose(); // Importiere die sqlite3-Bibliothek
+const bcrypt = require("bcrypt"); // Importiere die bcrypt-Bibliothek
 
 const dbPath = "./pro5.db";
 
+// Funktion zum Initialisieren der Datenbanken ... also der Tabellen (user, verlauf)
 async function initializeDatabase() {
   return new Promise((resolve, reject) => {
     const db = new sqlite3.Database(dbPath);
 
     db.run(
-      `CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)`,
+      `CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)`,
       (err) => {
         if (err) {
           reject(err);
         } else {
-          resolve();
+          db.run(
+            `CREATE TABLE IF NOT EXISTS verlauf (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, themaId INTEGER, frage TEXT, antwort TEXT, timestamp DATETIME)`,
+            (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve();
+              }
+              db.close();
+            }
+          );
         }
-        db.close();
       }
     );
   });
 }
 
-async function addUser(name, password) {
+
+
+// Ab hier die Funktionen für die Usertabelle
+
+// Funktion zum Hinzufügen eines Benutzers
+async function addUser(name, email, password) {
   return new Promise(async (resolve, reject) => {
     try {
       await initializeDatabase();
       const db = new sqlite3.Database(dbPath);
 
-      const stmt = db.prepare(
-        "INSERT INTO user (name, password) VALUES (?, ?)"
-      );
-      stmt.run(name, password, function (err) {
+      // Hashen des Passworts
+      bcrypt.hash(password, 10, function (err, hash) {
         if (err) {
           reject(err);
         } else {
-          resolve(this.lastID);
+          const stmt = db.prepare("INSERT INTO user (name, email, password) VALUES (?, ?, ?)");
+          stmt.run(name, email, hash, function (err) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(this.lastID);
+            }
+            stmt.finalize();
+            db.close();
+          });
         }
-        stmt.finalize();
-        db.close();
       });
     } catch (error) {
       reject(error);
@@ -44,6 +65,7 @@ async function addUser(name, password) {
   });
 }
 
+// Funktion zum Abrufen aller Benutzer
 async function getAllUsers() {
   return new Promise((resolve, reject) => {
     const db = new sqlite3.Database(dbPath);
@@ -59,6 +81,7 @@ async function getAllUsers() {
   });
 }
 
+// Funktion zum Löschen eines Benutzers (mit der jeweilige ID)
 async function deleteUser(id) {
   return new Promise(async (resolve, reject) => {
     try {
@@ -80,4 +103,48 @@ async function deleteUser(id) {
   });
 }
 
-module.exports = { addUser, getAllUsers, deleteUser };
+
+
+// Ab hier die Funktionen für die Verlaufstabelle
+
+// Funktion zum Hinzufügen eines Fragen/Antworten - Verlaufseintrags
+async function addHistoryEntry(userId, themaId, frage, antwort, timestamp) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const db = new sqlite3.Database(dbPath);
+
+      const stmt = db.prepare("INSERT INTO verlauf (userId, themaId, frage, antwort, timestamp) VALUES (?, ?, ?, ?, ?)");
+      stmt.run(userId, themaId, frage, antwort, timestamp, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+        stmt.finalize();
+        db.close();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// Funktion zum Abrufen des Fragen/Antworten - Verlaufs eines Benutzers
+async function getUserHistory(userId) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(dbPath);
+
+    db.all("SELECT * FROM verlauf WHERE userId = ?", [userId], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+      db.close();
+    });
+  });
+}
+
+
+// Export der Funktionen
+module.exports = { addUser, getAllUsers, deleteUser, addHistoryEntry, getUserHistory };
