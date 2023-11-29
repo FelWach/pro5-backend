939928c4da89e93f20da6a78cf12dae5fafcdaef
FelWach <s2110238071@fhooe.at>
2023-11-29 16:41:46 +0100

    GET /topics/:userId
    Responds with all the topics of a given user with userId

--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -110,4 +110,19 @@ router.get("/entries/:userId/:topic", async (req, res) => {
   }
 });
 
+router.get("/topics/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  if (!userId) {
+    return res.status(400).json({ message: "User ID required!" });
+  }
+
+  try {
+    const topics = await getTopic(userId);
+    res.json(topics);
+  } catch (error) {
+    res.status(500).json({ message: "Error while fetching topics!" });
+  }
+});
+
 module.exports = router;
